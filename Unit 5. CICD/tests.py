9ce7b161133bc6da997c3da50f630cd83b424c24
Anton Bevzyuk <anton.bevzyuk@raiffeisen.ru>
2022-01-22 09:53:50 +0300

    Парное программирование и тесты

--- Unit 5. CICD/tests.py
+++ Unit 5. CICD/tests.py
@@ -11,14 +11,5 @@ class TestStringMethods(unittest.TestCase):
     def test_has_header(self):
         self.assertTrue('<h1>Сказка про колобка</h1>' in self.html)
 
-    def test_has_rabbit(self):
-        self.assertTrue('<h3>3.1 Заяц</h3>' in self.html)
-        
-    def test_has_wolf(self):
-        self.assertTrue('<h3>3.2 Ночной Волк</h3>' in self.html)
-
-    def test_has_bear(self):
-        self.assertTrue('<h3>3.4 Медведь</h3>' in self.html)
-
 if __name__ == '__main__':
     unittest.main()
