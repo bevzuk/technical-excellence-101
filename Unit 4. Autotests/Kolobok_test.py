a081e3fd5d3bffe106ad3941edfd1ecace29a309
Anton Bevzyuk <anton.bevzyuk@raiffeisen.ru>
2022-01-22 10:34:54 +0300

    Fix linker errors

--- Unit 4. Autotests/Kolobok_test.py
+++ Unit 4. Autotests/Kolobok_test.py
@@ -2,10 +2,12 @@
 
 import pytest
 
+
 @pytest.fixture()
 def html():
     with open('./build/Kolobok.html', mode='r') as output_file:
         return output_file.read()
+
 
 def test_has_header(html):
     assert '<h1>Сказка про колобка</h1>' in html
--- Unit 5. CICD/tests.py
+++ Unit 5. CICD/tests.py
@@ -2,14 +2,16 @@
 
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
 
     def setUp(self):
-        with open('build/Kolobok.html',mode='r') as output_file:
+        with open('build/Kolobok.html', mode='r') as output_file:
             self.html = output_file.read()
 
     def test_has_header(self):
         self.assertTrue('<h1>Сказка про колобка</h1>' in self.html)
+
 
 if __name__ == '__main__':
     unittest.main()
