4b9dd82d333f9e0b658b8d9043c25261a3d3766c
Anton Bevzyuk <anton.bevzyuk@raiffeisen.ru>
2022-02-13 12:08:34 +0300

    Модуль 4. Автотесты

--- Unit 4. Autotests/Kolobok_test.py
+++ Unit 4. Autotests/Kolobok_test.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import os
 import pytest
 
 
 @pytest.fixture()
-def html():
-    with open('./build/Kolobok.html', mode='r') as output_file:
+def change_test_dir(request):
+    os.chdir(request.fspath.dirname)
+    yield
+    os.chdir(request.config.invocation_dir)
+
+
+@pytest.fixture()
+def text(change_test_dir):
+    with open('./Kolobok.md', mode='r') as output_file:
         return output_file.read()
 
 
-def test_has_header(html):
-    assert '<h1>Сказка про колобка</h1>' in html
+def test_has_header(text):
+    assert '# Сказка про колобка' in text
+
+def test_has_subheader_wolf(text):
+    assert '### 3.2 Собака серая' in text
