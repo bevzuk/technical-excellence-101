454038faa7dcf779718c500272afd384a5a47b57
Anton Bevzyuk <anton.bevzyuk@raiffeisen.ru>
2022-02-19 14:47:23 +0300

    Добавил тесты на Kolobok.html

---
+++ Unit 5. CICD/Kolobok_html_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+
+
+@pytest.fixture()
+def change_test_dir(request):
+    os.chdir(request.fspath.dirname)
+    yield
+    os.chdir(request.config.invocation_dir)
+
+
+@pytest.fixture()
+def html(change_test_dir):
+    with open('build/Kolobok.html', mode='r') as output_file:
+        return output_file.read()
+
+
+def test_has_header(html):
+    assert '<h1>Сказка про колобка</h1>' in html
+
+
+def test_has_subheader_wolf(html):
+    assert '<h3>3.2 Собака серая</h3>' in html
